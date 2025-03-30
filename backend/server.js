02202d323817561ed305265c2cf992bba12f5bbf
Rajdeep2302 <rajdeeppalofficial@gmail.com>
2025-03-30 16:12:42 +0530

    Code Updated

--- backend/server.js
+++ backend/server.js
@@ -2,4 +2,8 @@ const express = require('express');
 
 const app = express()
 
+app.get('/',(req, res) => {
+    res.send("API is Running");
+})
+
 app.listen(8080,console.log("Server started on http://localhost:8080/"))
